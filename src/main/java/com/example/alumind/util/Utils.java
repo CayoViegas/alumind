d9203f797615703848a985dea680beeb6ea4eed4
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2024-11-11 03:21:29 -0300

    feat: adicionar utils com função para carregamento do SYSTEM_PROMPT

---
+++ src/main/java/com/example/alumind/util/Utils.java
@@ -0,0 +1,18 @@
+package com.example.alumind.util;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Utils {
+    public static String loadSystemPrompt() {
+        try {
+            Path path = ResourceUtils.getFile("classpath:prompts/SYSTEM_PROMPT.txt").toPath();
+            return Files.readString(path);
+        } catch (IOException e) {
+            return "";
+        }
+    }
+}
