915a1aaf4525bd17a8e094651000c08002003651
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2024-11-11 15:57:44 -0300

    chore: remover anotações de dependências inexistentes

--- src/test/java/com/example/alumind/AluMindApplicationTests.java
+++ src/test/java/com/example/alumind/AluMindApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.alumind;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AluMindApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
